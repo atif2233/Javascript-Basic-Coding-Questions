6699d43155dc457a1c6146c22f6274cc0dad0ac3
atif2233 <136326436+atif2233@users.noreply.github.com>
2024-03-02 13:59:42 +0530

    Update 1 helloWorld.js

--- 1 helloWorld.js
+++ 1 helloWorld.js
@@ -1,19 +1,2 @@
 // write program to print hello world 
-
-// so there are many ways to print hello world 
-
-// 1 ->
-
 console.log("Hello World");
-
-
-// the below two ways are use to write hello world in html format within script tag
-
-// 2 -> alert is a function or a method which is used to create alert on web browser  
-
-// alert("Hello World");
-
-
-// 3 -> document.write is a function or a method of html which is used to write text on browser screen 
-
-// document.write("Hello World");
