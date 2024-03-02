ec49544c697377f490eb8b735252822ad298b9f5
atif2233 <136326436+atif2233@users.noreply.github.com>
2024-03-02 14:19:42 +0530

    Add comment's for code explanation

--- 2 addTwoNum.js
+++ 2 addTwoNum.js
@@ -2,5 +2,9 @@ function addNumbers(num1 , num2)
 {
     return num1 + num2;
 }
+// The return value of the function call (the sum of 4 and 5) is stored in a variable named result and then print result in next line.
+const result = addNumbers(4,5);
+console.log(result);
 
+// The function addNumbers is called with arguments 4 and 5, and its return value (the sum of 4 and 5) is immediately passed to console.log() for printing to the console.
 console.log(addNumbers(4,5));
