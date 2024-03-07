6d29dc0bbdfabdbfe71285a2c8be7269fed2fee2
atif2233 <136326436+atif2233@users.noreply.github.com>
2024-03-07 17:50:46 +0530

    Add javascript code to find square root.
    This file contains JavaScript code to calculate the square root of a number using Math.sqrt() method.

---
+++ 4 squareRoot.js
@@ -0,0 +1,20 @@
+// write a program to find square root of a number 
+
+// first what is square root 
+// The square root of a number is a value that, when multiplied by itself, gives the original number.  
+// The square root of 9 is 3, because 3X3 = 9 
+
+// second what is square 
+// The square of a number is the result of multiplying the number by itself. 
+// The square of 2 is 2×2 = 4
+
+// find square root of number in javscript 
+// there is a built in method Math.sqrt() in javascript to find square root of number 
+
+var a = 9;
+console.log(Math.sqrt(a)); // 3 
+console.log(Math.sqrt(81)); // 9
+console.log(Math.sqrt(26)); // give output in decimal because 26 is not a perfect square therefore it will give accurate value by calculating square root
+console.log(Math.sqrt("hello")); // give output NaN -> not a number because alphabet is not consider as number 
+
+
